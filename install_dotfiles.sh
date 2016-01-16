d972eb644e1570f269ff82e7e3e6e8f2875ccce7
Patrick McKenna <patrickmckenna@github.com>
2016-01-15 16:24:46 -0800

    script symlinking dotfiles

---
+++ install_dotfiles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+DOTFILESDIR=$(dirname $0)
+cd $DOTFILESDIR
+DOTFILESDIR=$(pwd -P)
+
+for DOTFILE in *; do
+  echo "$DOTFILE" | egrep -q '(\.sublime-settings|\.sh)' && continue
+
+  TARGETFILE="$HOME/.$DOTFILE"
+  [ "$DOTFILE" = "gitignore" ] && TARGETFILE="$TARGETFILE"_global
+  DOTFILEPATH="$DOTFILESDIR/$DOTFILE"
+  
+  if [ -f "$TARGETFILE" ]
+  then
+    echo "==> file $TARGETFILE already exists, overwriting it"
+    rm "$TARGETFILE"
+  fi
+  ln -s "$DOTFILEPATH" "$TARGETFILE" 
+
+done
